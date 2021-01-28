322ca9d9df1da2405b47b769bb28c44455bcaeda
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2021-01-28 14:23:48 -0600

    started

---
+++ StudentExampleReview/StudentExampleReview/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace StudentExampleReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("studentdataRandom100Rows.csv");
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+
+        }
+    }
+}
---
+++ StudentExampleReview/StudentExampleReview/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentExampleReview
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteColor { get; set; }
+        public Student()
+        {
+            ID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+        public Student(int id, string first, string last, string color)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            FavoriteColor = color;
+        }
+        public string CreateFullName()
+        {
+            return $"{LastName}, {FirstName}";
+        }
+    }
+}
