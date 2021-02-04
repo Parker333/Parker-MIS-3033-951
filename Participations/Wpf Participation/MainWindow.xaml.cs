e40bdac142b6a3abe91e25cb1d41a8f74d8f386c
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2021-02-04 14:28:45 -0600

    Started on my participation

---
+++ Participations/Wpf Participation/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Participation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtName.Clear();
+        }
+
+        private void btnStart_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Please enter your information", "Clicked", MessageBoxButton.OKCancel);
+
+            string birthday = txtBirthday.Text;
+            DateTime result = Convert.ToDateTime(birthday);
+            DateTime now = DateTime.Now;
+            TimeSpan age = now - result;
+
+            MessageBox.Show ($"{txtName} is {age.Days/365}.");
+            lblOutput.Content = ($"{txtName} is {age.Days/365}.");
+        }
+    }
+}
