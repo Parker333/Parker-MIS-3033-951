deaa3e041245a34594aaaf156f49266f5db9f5c6
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2021-02-12 18:38:37 -0600

    No idea

---
+++ Participations/Wpf1-Classes/Info.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wpf1_Classes
+{
+   public class Info
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Zipcode { get; set; }
+
+        public Info()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            Zipcode = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} \n Address: {Address} \n Zipcode: {Zipcode}";
+        }
+    }
+}
---
+++ Participations/Wpf1-Classes/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf1_Classes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Info myinfo = new Info()
+            {
+                Name = txtName.Text,
+                Address = txtAddress.Text,
+                Zipcode = Convert.ToInt32(txtZipcode)
+            };
+
+            lstInfo.Items.Add(myinfo);
+            txtName.Clear();
+            txtAddress.Clear();
+            txtZipcode.Clear();
+        }
+
+        private void lstInfo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
