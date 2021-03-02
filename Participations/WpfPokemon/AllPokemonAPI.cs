30c03eaf3797869fd08283e775d4ddb6a59b84f8
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2021-03-02 00:27:02 -0600

    I have no idea what I'm doing

---
+++ Participations/WpfPokemon/AllPokemonAPI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfPokemon
+{
+    public class AllPokemonAPI
+    {
+        public List<ResultObject> results { get; set; }
+        public Info info { get; set; }
+
+    }
+
+    public class ResultObject
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string sprites { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+    }
+
+    public class Info
+    {
+        public int height { get; set; }
+        public int weight { get; set; }
+        public string sprites { get; set; }
+    }
+}
