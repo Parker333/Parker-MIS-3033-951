bf8f2b802414fcdb0322fa1bfbe5ba1da7556950
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2021-04-16 01:16:05 -0500

    finished

---
+++ Participations/Beginner/Models/PokemonInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Beginner.Models
+{
+    public class PokemonInfo
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string height { get; set; }
+        public string weight { get; set; }
+        public Sprites sprites { get; set; }
+    }
+    public class Sprites
+    {
+        public string back_default { get; set; }
+        public string front_default { get; set; }
+
+    }
+}
